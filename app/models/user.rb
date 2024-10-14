bcce9f1d67b5c186c429ed60a2e308809878124d
Sanket Bodke <84568517+sanketbodke@users.noreply.github.com>
2024-10-14 22:48:35 +0530

    rolify gem add for set user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  rolify
+
+  after_create :assign_default_role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :email, presence: true
+
+  def assign_default_role
+    self.add_role(:newuser) if self.roles.blank?
+  end
 end
