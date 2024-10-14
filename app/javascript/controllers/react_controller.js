294eec8dd86f72bdb942500a50216a05926bf604
Sanket Bodke <84568517+sanketbodke@users.noreply.github.com>
2024-10-14 13:12:23 +0530

    setup

---
+++ app/javascript/controllers/react_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+import React from "react";
+import ReactDom from "react-dom"
+import App from './App'
+
+// Connects to data-controller="react"
+export default class extends Controller {
+  connect() {
+    console.log("connected")
+
+    const root = ReactDom.createRoot(document.getElementById("app"))
+    root.render(
+        <App/>
+    )
+  }
+}
