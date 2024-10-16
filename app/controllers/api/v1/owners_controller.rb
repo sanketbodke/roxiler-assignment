4c8033f9da3fef98a1f593267491b202fb80e0b6
Sanket Bodke <84568517+sanketbodke@users.noreply.github.com>
2024-10-17 00:54:57 +0530

    owner controller add

---
+++ app/controllers/api/v1/owners_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+    class OwnersController < BaseController
+      def index
+        @owners = User.joins(:roles)
+                      .where(roles: { name: 'owner' })
+                      .distinct
+
+        render json: @owners.as_json(include: { roles: { only: [:name] } }), status: :ok
+      end
+
+
+      def show
+        user = User.find(params[:id])
+        store = user.stores
+
+        if store
+          render json: store.as_json(include: { ratings: { only: [:id, :score] } }), status: :ok
+        else
+          render json: { error: "Store not found" }, status: :not_found
+        end
+      end
+    end
+  end
+end
