67bb53a89386b2a4675bd5bbfdef024c9ce7fa7a
Sanket Bodke <84568517+sanketbodke@users.noreply.github.com>
2024-10-16 14:56:36 +0530

    fix sidebar items

--- app/javascript/components/Sidebar/Sidebar.component.jsx
+++ app/javascript/components/Sidebar/Sidebar.component.jsx
@@ -1,41 +1,70 @@
 import React from 'react';
 import { SidebarContainer, SidebarItems, SidebarLink } from "./sidebar.styled";
 import useSidebar from "./useSidebar";
+import currentUser from "../../utils/currentUser";
 
 function Sidebar() {
-    const { activeTab, handleTabClick , handleLogout} = useSidebar();
+    const { activeTab, handleTabClick, handleLogout } = useSidebar();
+    const { user } = currentUser();
+    const roles = user.data.role;
+
+    const isUserRole = roles.includes('user') || roles.includes('newuser');
 
     return (
         <SidebarContainer>
             <SidebarItems>
-                <SidebarLink
-                    to="/"
-                    onClick={() => handleTabClick('/')}
-                    className={activeTab === '/' ? 'active' : ''}
-                >
-                    Home
-                </SidebarLink>
-                <SidebarLink
-                    to="/users/all"
-                    onClick={() => handleTabClick('/users/all')}
-                    className={activeTab === '/users/all' ? 'active' : ''}
-                >
-                    Users
-                </SidebarLink>
-                <SidebarLink
-                    to="/stores/all"
-                    onClick={() => handleTabClick('/stores/all')}
-                    className={activeTab === '/stores/all' ? 'active' : ''}
-                >
-                    Stores
-                </SidebarLink>
-                <SidebarLink
-                    to="/"
-                    onClick={() => handleLogout()}
-                    className={activeTab === '/logout' ? 'active' : ''}
-                >
-                    Logout
-                </SidebarLink>
+                {isUserRole ? (
+                    <>
+                        <SidebarLink
+                            to="/stores/all"
+                            onClick={() => handleTabClick('/stores/ratings')}
+                            className={activeTab === '/stores/ratings' ? 'active' : ''}
+                        >
+                            Store Ratings
+                        </SidebarLink>
+                        <SidebarLink
+                            to="/"
+                            onClick={() => handleLogout()}
+                            className={activeTab === '/logout' ? 'active' : ''}
+                        >
+                            Logout
+                        </SidebarLink>
+                    </>
+                ) : (
+                    <>
+                        <SidebarLink
+                            to="/"
+                            onClick={() => handleTabClick('/')}
+                            className={activeTab === '/' ? 'active' : ''}
+                        >
+                            Home
+                        </SidebarLink>
+
+                        <SidebarLink
+                            to="/users/all"
+                            onClick={() => handleTabClick('/users/all')}
+                            className={activeTab === '/users/all' ? 'active' : ''}
+                        >
+                            Users
+                        </SidebarLink>
+
+                        <SidebarLink
+                            to="/stores/all"
+                            onClick={() => handleTabClick('/stores/all')}
+                            className={activeTab === '/stores/all' ? 'active' : ''}
+                        >
+                            Stores
+                        </SidebarLink>
+
+                        <SidebarLink
+                            to="/"
+                            onClick={() => handleLogout()}
+                            className={activeTab === '/logout' ? 'active' : ''}
+                        >
+                            Logout
+                        </SidebarLink>
+                    </>
+                )}
             </SidebarItems>
         </SidebarContainer>
     );
